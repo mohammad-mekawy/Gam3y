9e40648da55679764982fd7fc0be69b9323fa875
mohammad-mekawy <m_mekawy92@yahoo.com>
2023-07-29 13:15:00 +0300

    up BTN

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
-// Up BTN
+document.addEventListener('DOMContentLoaded', function() {
+
 var myUpButton = document.querySelector('div.up');
+var navbar = document.querySelector('.navbar');
+
 window.onscroll = function() {
+    window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
     window.scrollY > 120 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
 };
-myUpButton.style.opacity = window.scrollY > 120 ? '1' : '0';
+window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
+window.scrollY > 120 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
 
 myUpButton.onclick = function() {
     window.scrollTo({
@@ -11,10 +16,5 @@ myUpButton.onclick = function() {
         behavior: 'smooth'
     });
 };
-// navbar shadow
-var navbar = document.querySelector('.navbar');
 
-window.onscroll = function() {
-    window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
-};
-window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
+});
