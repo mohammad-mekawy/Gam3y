f03589a3595bf900782c977f8aec36ad85913239
mohammad-mekawy <m_mekawy92@yahoo.com>
2023-08-02 11:31:45 +0300

    version 1.01

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,13 @@ var myUpButton = document.querySelector('div.up');
 var navbar = document.querySelector('.navbar');
 
 window.onscroll = function() {
-    window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
-    window.scrollY > 120 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
-    window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
-    window.scrollY > 120 ? myUpButton.style.transform = 'translateY(-20px)' : myUpButton.style.transform = 'translateY(0)';
+    window.scrollY > 40 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
+    window.scrollY > 250 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
+    window.scrollY > 40 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
+    window.scrollY > 250 ? myUpButton.style.transform = 'translateY(-20px)' : myUpButton.style.transform = 'translateY(0)';
 };
-window.scrollY > 120 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
-window.scrollY > 120 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
+window.scrollY > 250 ? navbar.classList.add('shadow') : navbar.classList.remove('shadow');
+window.scrollY > 250 ? myUpButton.style.opacity = '1' : myUpButton.style.opacity = '0';
 
 myUpButton.onclick = function() {
     window.scrollTo({
